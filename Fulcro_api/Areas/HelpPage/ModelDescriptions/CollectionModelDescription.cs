a0502129d8a7fcdfcf839d929d63da9229925404
Ravi32Raja <ravi32sag@gmail.com>
2022-06-06 10:55:06 +0545

    API_code

---
+++ Fulcro_api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Fulcro_api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
